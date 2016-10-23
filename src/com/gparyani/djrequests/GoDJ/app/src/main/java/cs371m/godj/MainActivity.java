ee4d914b3b3d3c8b0ac54f16b8ff2478758e50d8
LAPTOP-6RB3F751\Jasmine <jasmine.green@utexas.edu>
2016-10-22 23:47:54 -0500

    "added sdk"

--- src/com/gparyani/djrequests/GoDJ/app/src/main/java/cs371m/godj/MainActivity.java
+++ src/com/gparyani/djrequests/GoDJ/app/src/main/java/cs371m/godj/MainActivity.java
@@ -4,14 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.SettableFuture;
-import com.wrapper.spotify.Api;
-import com.wrapper.spotify.models.Artist;
-import com.wrapper.spotify.models.Page;
-
 import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,24 +24,52 @@ public class MainActivity extends AppCompatActivity {
         final String clientId = "6e8cc80c1cd6419484b88a02348aa7e4";
         final String clientSecret = "a06668cfb70643c8b497a3cfd1dd90d9";
 
-        final Api api = Api.builder()
-                .clientId(clientId)
-                .clientSecret(clientSecret)
-                .build();
-        SettableFuture<Page<Artist>> artistSettableFuture = api.searchArtists("usher").market("US").limit(10).build().getAsync();
-        Futures.addCallback(artistSettableFuture, new FutureCallback<Page<Artist>>() {
+        SpotifyApi api = new SpotifyApi();
+
+// Most (but not all) of the Spotify Web API endpoints require authorisation.
+// If you know you'll only use the ones that don't require authorisation you can skip this step
+        //api.setAccessToken("myAccessToken");
+
+        SpotifyService spotify = api.getService();
+
+        spotify.searchArtists("usher", new Callback<ArtistsPager>() {
             @Override
-            public void onSuccess(Page<Artist> artistPage) {
-                List<Artist> artists = artistPage.getItems();
+            public void success(ArtistsPager artistsPager, Response response) {
                 //display artists in list
+                List<Artist> artists = artistsPager.artists.items;
+
+
+
+
+                
                 Log.d("artists:", artists.toString());
             }
 
             @Override
-            public void onFailure(Throwable throwable) {
-                Log.d("artists:", "ERROR:" + throwable.getMessage());
+            public void failure(RetrofitError error) {
+               Log.d("artists:", "ERROR:" + error.getMessage());
 
             }
         });
+
+//        final Api api = Api.builder()
+//                .clientId(clientId)
+//                .clientSecret(clientSecret)
+//                .build();
+//        SettableFuture<Page<Artist>> artistSettableFuture = api.searchArtists("usher").market("US").limit(10).build().getAsync();
+//        Futures.addCallback(artistSettableFuture, new FutureCallback<Page<Artist>>() {
+//            @Override
+//            public void onSuccess(Page<Artist> artistPage) {
+//                List<Artist> artists = artistPage.getItems();
+//                //display artists in list
+//                Log.d("artists:", artists.toString());
+//            }
+//
+//            @Override
+//            public void onFailure(Throwable throwable) {
+//                Log.d("artists:", "ERROR:" + throwable.getMessage());
+//
+//            }
+//        });
     }
 }
