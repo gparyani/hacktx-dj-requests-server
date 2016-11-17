f2e8a0d1836d1ab4a2df3f7107b0037e324fe6ac
Rebecca Lemma <reblem53@utexas.edu>
2016-11-17 13:31:43 -0600

    Added artist's name to album search item

--- src/com/gparyani/djrequests/GoDJ/app/src/main/java/cs371m/godj/AlbumItemAdapter.java
+++ src/com/gparyani/djrequests/GoDJ/app/src/main/java/cs371m/godj/AlbumItemAdapter.java
@@ -1,6 +1,7 @@
 package cs371m.godj;
 
 import android.content.Context;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +13,15 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Album;
 import kaaes.spotify.webapi.android.models.AlbumSimple;
+import kaaes.spotify.webapi.android.models.ArtistSimple;
 import kaaes.spotify.webapi.android.models.Image;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 /**
  * Created by rebeccal on 11/10/16.
@@ -23,9 +31,11 @@ public class AlbumItemAdapter extends BaseAdapter {
 
     private LayoutInflater inflater;
     private List<AlbumSimple> albumList;
+    private Handler myHandler;
 
     public AlbumItemAdapter(Context context) {
         inflater = LayoutInflater.from(context);
+        myHandler = new Handler();
     }
 
     @Override
@@ -55,25 +65,53 @@ public class AlbumItemAdapter extends BaseAdapter {
         }
     }
 
-    public void bindView(AlbumSimple data, View view, ViewGroup parent) {
+    public void bindView(AlbumSimple data, final View view, ViewGroup parent) {
         ImageView albumImage = (ImageView) view.findViewById(R.id.album_pic);
         TextView albumName = (TextView) view.findViewById(R.id.name_of_album);
         TextView albumID = (TextView) view.findViewById(R.id.album_id);
-        // TextView artistName = (TextView) view.findViewById(R.id.name_of_artist);
 
-        // change image
         List<Image> images = data.images;
         if (images.size() > 0) {
             String imageURL = images.get(0).url;
             Picasso.with(view.getContext()).load(imageURL).into(albumImage);
         } else {
-            albumImage.setImageResource(R.drawable.microphone);
+            albumImage.setImageResource(R.drawable.music_record);
         }
+
         albumName.setText(data.name);
         albumID.setText(data.id);
 
+        SpotifyApi api = new SpotifyApi();
+        SpotifyService spotify = api.getService();
+        spotify.getAlbum(data.id, new Callback<Album>() {
+            @Override
+            public void success(Album album, Response response) {
+                myHandler.post(new AlbumSelected(album, view));
+            }
 
+            @Override
+            public void failure(RetrofitError error) {
 
+            }
+        });
+    }
+
+    private class AlbumSelected implements Runnable {
+
+        private Album album;
+        private View view;
+
+        public AlbumSelected(Album album, View view) {
+            this.album = album;
+            this.view = view;
+        }
+
+        @Override
+        public void run() {
+            TextView artistName = (TextView) view.findViewById(R.id.name_of_artist);
+            ArtistSimple artist = album.artists.get(0);
+            artistName.setText(artist.name);
+        }
     }
 
     @Override
