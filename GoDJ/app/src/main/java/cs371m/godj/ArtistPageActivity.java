3bf9fd4b984d7903f42c7ee31181eaa03d873389
Rebecca Lemma <reblem53@utexas.edu>
2016-11-29 18:33:08 -0600

    Completed artist page, still need to limit results

--- GoDJ/app/src/main/java/cs371m/godj/ArtistPageActivity.java
+++ GoDJ/app/src/main/java/cs371m/godj/ArtistPageActivity.java
@@ -2,6 +2,7 @@ package cs371m.godj;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
@@ -12,7 +13,17 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Album;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.Artists;
+import kaaes.spotify.webapi.android.models.Pager;
 import kaaes.spotify.webapi.android.models.Track;
+import kaaes.spotify.webapi.android.models.Tracks;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 
 /**
@@ -29,14 +40,18 @@ public class ArtistPageActivity extends AppCompatActivity {
     private TextView relatedTitle;
 
     private ListView popularTrackListView;
-    private ListView albumListView;
+    private ListView albumsListView;
     private ListView relatedArtistListView;
 
     private List<Track> popularTracks;
-//    private List<Album> albums;
-//    private List<Artist> relatedArtists;
+    private List<Album> albums;
+    private List<Artist> relatedArtists;
 
     private SpotifyItemAdapter popTracksAdapter;
+    private AlbumItemAdapter albumsItemAdapter;
+    private ArtistItemAdapter relatedArtistsAdapter;
+
+    private Handler myHandler;
 
 
     @Override
@@ -47,37 +62,146 @@ public class ArtistPageActivity extends AppCompatActivity {
         artistImage = (ImageView) findViewById(R.id.artist_image);
         artistName = (TextView) findViewById(R.id.artist_nm);
         popularTrackListView = (ListView) findViewById(R.id.popular_tracks);
-        albumListView = (ListView) findViewById(R.id.albums);
+        albumsListView = (ListView) findViewById(R.id.albums);
         relatedArtistListView = (ListView) findViewById(R.id.related_artists);
+        myHandler = new Handler();
 
         popularTitle = new TextView(this);
         popularTitle.setText("Popular");
+        popularTitle.setTextColor(0xffffffff);
+        popularTitle.setTextSize(20);
+        popularTitle.setTypeface(popularTitle.getTypeface(), 1);
+        popularTitle.setPadding(350,35,0,50);
+        popularTrackListView.addHeaderView(popularTitle, null, false);
+        popularTitle.setVisibility(View.INVISIBLE);
 
         albumTitle = new TextView(this);
         albumTitle.setText("Albums");
+        albumTitle.setTextColor(0xffffffff);
+        albumTitle.setTextSize(20);
+        albumTitle.setTypeface(albumTitle.getTypeface(), 1);
+        albumTitle.setPadding(350,0,0,50);
+        albumsListView.addHeaderView(albumTitle, null, false);
+        albumTitle.setVisibility(View.INVISIBLE);
 
         relatedTitle = new TextView(this);
         relatedTitle.setText("Related Artists");
+        relatedTitle.setTextColor(0xffffffff);
+        relatedTitle.setTextSize(20);
+        relatedTitle.setTypeface(relatedTitle.getTypeface(), 1);
+        relatedTitle.setPadding(270,0,0,50);
+        relatedArtistListView.addHeaderView(relatedTitle, null, false);
+        relatedTitle.setVisibility(View.INVISIBLE);
 
-        popularTitle.setTextColor(0xffffffff);
-        // popularTitle.setGravity(0x01);
-        popularTitle.setTextSize(20);
-        popularTitle.setTypeface(popularTitle.getTypeface(), 1);
-        popularTitle.setPadding(0,35,0,50);
-        popularTrackListView.addHeaderView(popularTitle, null, false);
-        popularTitle.setVisibility(View.VISIBLE);
+        popTracksAdapter = new SpotifyItemAdapter(this);
+        albumsItemAdapter = new AlbumItemAdapter(this);
+        relatedArtistsAdapter = new ArtistItemAdapter(this);
 
+        popularTrackListView.setAdapter(popTracksAdapter);
+        albumsListView.setAdapter(albumsItemAdapter);
+        relatedArtistListView.setAdapter(relatedArtistsAdapter);
+
+
+        callServices();
+    }
+
+
+    class TopTracks implements Runnable {
+
+        @Override
+        public void run() {
+            popularTitle.setVisibility(View.VISIBLE);
+            popTracksAdapter.notifyDataSetChanged();
+            popTracksAdapter.notifyDataSetChanged();
+            popTracksAdapter.changeList(popularTracks);
+            UserMainActivity.ListUtils.setDynamicHeight(popularTrackListView);
+            popTracksAdapter.notifyDataSetChanged();
+        }
+    }
+
+    class ArtistAlbums implements Runnable {
+
+        @Override
+        public void run() {
+            albumTitle.setVisibility(View.VISIBLE);
+            albumsItemAdapter.notifyDataSetChanged();
+            albumsItemAdapter.notifyDataSetChanged();
+            albumsItemAdapter.changeList(albums);
+            UserMainActivity.ListUtils.setDynamicHeight(albumsListView);
+            albumsItemAdapter.notifyDataSetChanged();
+
+        }
+    }
+
+    class RelatedArtists implements Runnable {
+
+        @Override
+        public void run() {
+            relatedTitle.setVisibility(View.VISIBLE);
+            relatedArtistsAdapter.notifyDataSetChanged();
+            relatedArtistsAdapter.notifyDataSetChanged();
+            relatedArtistsAdapter.changeList(relatedArtists);
+            UserMainActivity.ListUtils.setDynamicHeight(relatedArtistListView);
+            relatedArtistsAdapter.notifyDataSetChanged();
+
+        }
+    }
+
+
+
+    protected void callServices() {
         Intent intent = getIntent();
-
-        popularTracks = intent.getParcelableArrayListExtra("popTrackList");
+        String artistID = intent.getStringExtra("artistID");
         Picasso.with(getApplicationContext()).load(intent.getStringExtra("artistURL")).into(artistImage);
         artistName.setText(intent.getStringExtra("artistName"));
 
-        popTracksAdapter = new SpotifyItemAdapter(this);
-        popularTrackListView.setAdapter(popTracksAdapter);
-        popTracksAdapter.changeList(popularTracks);
-        popTracksAdapter.notifyDataSetChanged();
+
+        SpotifyApi api = new SpotifyApi();
+        SpotifyService spotify = api.getService();
+
+        spotify.getArtistTopTrack(artistID, "US", new Callback<Tracks>() {
+            @Override
+            public void success(Tracks tracks, Response response) {
+                popularTracks = tracks.tracks;
+                myHandler.post(new TopTracks());
 
 
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
+
+        spotify.getArtistAlbums(artistID, new Callback<Pager<Album>>() {
+            @Override
+            public void success(Pager<Album> albumPager, Response response) {
+                albums = albumPager.items;
+                myHandler.post(new ArtistAlbums());
+
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
+
+        spotify.getRelatedArtists(artistID, new Callback<Artists>() {
+            @Override
+            public void success(Artists artists, Response response) {
+                relatedArtists = artists.artists;
+                myHandler.post(new RelatedArtists());
+
+
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
     }
+
 }
