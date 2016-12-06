367fab21a6bf27fe9d3eb984da0bbf160819623d
LAPTOP-6RB3F751\Jasmine <jasmine.green@utexas.edu>
2016-12-06 10:11:18 -0600

    can search by date

--- GoDJ/app/src/main/java/cs371m/godj/EventSearchFragment.java
+++ GoDJ/app/src/main/java/cs371m/godj/EventSearchFragment.java
@@ -5,15 +5,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.support.design.widget.Snackbar.make;
 
 /**
  * Created by Jasmine on 11/29/2016.
@@ -23,6 +28,8 @@ public class EventSearchFragment extends Fragment {
 
     protected Button searchBut;
     protected EditText searchET;
+    protected EditText dateET;
+    protected Button dateBut;
     protected List<EventObject> events;
     protected Handler handler;
 
@@ -45,6 +52,15 @@ public class EventSearchFragment extends Fragment {
         View v = inflater.inflate(R.layout.event_search_layout, container, false);
         searchBut = (Button) v.findViewById(R.id.event_search_but);
         searchET = (EditText) v.findViewById(R.id.event_search_term);
+        dateET = (EditText) v.findViewById(R.id.date_event_search_term);
+        dateET.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new SearchDatePickerFragment();
+                newFragment.show(getFragmentManager(), "searchDatePicker");
+            }
+        });
+        dateBut = (Button) v.findViewById(R.id.date_event_search_but);
         events = new ArrayList<>();
         handler = new Handler();
 
@@ -54,6 +70,8 @@ public class EventSearchFragment extends Fragment {
             returnToSearch = false;
         }
         startingEventFind = false;
+
+
 
 
         return v;
@@ -66,15 +84,50 @@ public class EventSearchFragment extends Fragment {
             @Override
             public void onClick(View v) {
 
+
                 startingEventFind = true;
                 Bundle b = new Bundle();
-                b.putString("searchTerm", searchET.getText().toString());
-                EventSearchResultsFragment esrf = new EventSearchResultsFragment();
-                esrf.setArguments(b);
-                getFragmentManager().beginTransaction()
-                        .replace(R.id.main_frame, esrf, "eventSearch")
-                        .addToBackStack("eventSearch")
-                        .commit();
+                if(!(searchET.getText().toString().equals(""))) {
+                    b.putString("searchTerm", searchET.getText().toString());
+                    EventSearchResultsFragment esrf = new EventSearchResultsFragment();
+                    esrf.setArguments(b);
+                    getFragmentManager().beginTransaction()
+                            .replace(R.id.main_frame, esrf, "eventSearch")
+                            .addToBackStack("eventSearch")
+                            .commit();
+                } else {
+                    ViewGroup viewGroup = (ViewGroup) ((ViewGroup) getActivity().findViewById(android.R.id.content)).getChildAt(0);
+                    Snackbar snack = make(viewGroup, "Please enter a search term first", Snackbar.LENGTH_LONG);
+                    View view = snack.getView();
+                    TextView tv = (TextView) view.findViewById(android.support.design.R.id.snackbar_text);
+                    tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                    snack.show();
+                }
+
+            }
+        });
+
+        dateBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startingEventFind = true;
+                Bundle b = new Bundle();
+                if (!(dateET.getText().toString().equals(""))) {
+                    b.putString("dateTerm", dateET.getText().toString());
+                    EventSearchResultsFragment esrf = new EventSearchResultsFragment();
+                    esrf.setArguments(b);
+                    getFragmentManager().beginTransaction()
+                            .replace(R.id.main_frame, esrf, "eventSearch")
+                            .addToBackStack("eventSearch")
+                            .commit();
+                } else {
+                    ViewGroup viewGroup = (ViewGroup) ((ViewGroup) getActivity().findViewById(android.R.id.content)).getChildAt(0);
+                    Snackbar snack = make(viewGroup, "Please enter a search term first", Snackbar.LENGTH_LONG);
+                    View view = snack.getView();
+                    TextView tv = (TextView) view.findViewById(android.support.design.R.id.snackbar_text);
+                    tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                    snack.show();
+                }
             }
         });
     }
